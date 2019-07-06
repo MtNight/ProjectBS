6aad9168960f4e3773d3066b808c3e1fc985ad94
Mt.Night <eskwon0228@naver.com>
2019-07-06 22:36:26 +0900

    Unity Test

---
+++ Works/ProjectBS/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    private Vector3 moveVec;
+    private float moveSpeed;
+
+	void Start () {
+        moveVec = new Vector3(0, 0, 0);
+        moveSpeed = 2.0f;
+	}
+	
+	void Update ()
+    {
+        moveVec = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVec += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVec += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVec += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVec += Vector3.right;
+        }
+
+        transform.position += moveVec * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ Works/ProjectBS/Assets/Scripts/TestRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotate : MonoBehaviour {
+
+    float angle;
+
+	void Start () {
+        angle = 0;
+    }
+	
+	void Update () {
+        if (angle < 10)
+        {
+            angle += Time.deltaTime;
+        }
+        Vector3 rot = new Vector3(0, angle, 0);
+        transform.Rotate(rot);
+	}
+}
